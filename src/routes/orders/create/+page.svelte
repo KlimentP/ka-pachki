176fd06a0f5f047a201d14d703b921f8db6d86d6
Kliment Preshlenov <klimen.preshlenov@gmail>
2023-04-23 22:35:49 +0300

    drop auto-generated fields from html

--- src/routes/orders/create/+page.svelte
+++ src/routes/orders/create/+page.svelte
@@ -1,7 +1,7 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
     <form class="w-full max-w-lg">
-        <div class="flex flex-wrap -mx-3 mb-6">
+        <!-- <div class="flex flex-wrap -mx-3 mb-6">
           <div class="w-full px-3">
             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="date-updated">
               Date Updated
@@ -16,13 +16,13 @@
             </label>
             <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="date-closed" type="date" placeholder="Date Closed">
           </div>
-        </div>
+        </div> -->
         <div class="flex flex-wrap -mx-3 mb-6">
           <div class="w-full px-3">
             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="qty">
-              QTY
+              Quantity
             </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="qty" type="number" placeholder="QTY">
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="qty" type="number" placeholder="Quantity">
           </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
@@ -78,22 +78,22 @@
               <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="units-produced" type="number" placeholder="Units Produced">
             </div>
           </div>
-          <div class="flex flex-wrap -mx-3 mb-6">
+          <!-- <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full px-3">
               <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="created-by">
                 Created By
               </label>
               <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="created-by" type="text" placeholder="Created By">
             </div>
-          </div>
-          <div class="flex flex-wrap -mx-3 mb-6">
+          </div> -->
+          <!-- <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full px-3">
               <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="closed-by">
                 Closed By
               </label>
               <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="closed-by" type="text" placeholder="Closed By">
             </div>
-          </div>
+          </div> -->
           <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full px-3">
               <div class="relative">
@@ -123,13 +123,13 @@
                         </div>
                       </div>
                       <div class="flex flex-wrap -mx-3 mb-6">
-                        <div class="w-full px-3">
+                        <!-- <div class="w-full px-3">
                           <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="order-id">
                             Order ID
                           </label>
                           <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="order-id" type="text" placeholder="Order ID">
                         </div>
-                      </div>
+                      </div> -->
                       <div class="flex flex-wrap -mx-3 mb-2">
                         <div class="w-full px-3">
                           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
