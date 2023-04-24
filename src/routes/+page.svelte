08a636ac33f0bc8a0cb551a8b01e37624bf147f5
Kliment Preshlenov <klimen.preshlenov@gmail>
2023-04-24 19:54:04 +0300

    put picture in home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,10 +10,17 @@
 		{#if data.session}
 			<div class="container h-full mx-auto flex justify-center items-center">
 				<div class="space-y-5">
-					<h1>Hello {data.session.user.email}</h1>
+					<h1 class="mb-2">Hello {data.session.user.email}</h1>
+				</div>
+			</div>
+		{:else}
+			<div class="container h-full mx-auto flex justify-center items-center">
+				<div class="space-y-5">
+					<h1 class="mb-2">Hello Guest</h1>
 				</div>
 			</div>
 		{/if}
+		<img class="rounded-xl shadow-lg"src="hero.png" alt="Magic printing press"/>
 	</div>
 </div>
 
