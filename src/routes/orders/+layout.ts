5a451c353a7a3d70ce0890e0747ae73a7d321d53
Kliment Preshlenov <klimen.preshlenov@gmail>
2023-04-24 19:28:10 +0300

    fix types

--- src/routes/orders/+layout.ts
+++ src/routes/orders/+layout.ts
@@ -1,7 +1,7 @@
 import { redirect } from '@sveltejs/kit';
-import type { PageLoad } from './$types';
+import type { LayoutLoad } from './$types';
 
-export const load: PageLoad = async ({ parent }) => {
+export const load: LayoutLoad = async ({ parent }) => {
 	const { session } = await parent();
 	if (!session) {
 		throw redirect(303, '/');
