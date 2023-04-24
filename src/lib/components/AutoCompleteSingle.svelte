45b35659ee6c1d94d4aac1babda42cb150b0bc90
Kliment Preshlenov <klimen.preshlenov@gmail>
2023-04-24 11:49:12 +0300

    split autocomplete components into two versions

---
+++ src/lib/components/AutoCompleteSingle.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { Autocomplete } from '@skeletonlabs/skeleton';
+	import type { AutocompleteOption } from '@skeletonlabs/skeleton';
+
+let input = ''
+export let options: AutocompleteOption[] = [];
+function onSelection(event: any): void {
+	input = event.detail.label;
+}
+
+
+</script>
+
+
+<input class="input" type="search" name="demo" bind:value={input} placeholder="Search..." />
+<div class="card w-full  max-h-48 p-4 overflow-y-auto">
+	<Autocomplete bind:input={input} options={options} on:selection={onSelection} />
+</div>
