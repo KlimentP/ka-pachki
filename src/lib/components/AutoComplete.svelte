ab8aec83fd6cbeb51662f823af3b15f6562ccc9e
Kliment Preshlenov <klimen.preshlenov@gmail>
2023-04-24 11:24:07 +0300

    add autocomplete component

---
+++ src/lib/components/AutoComplete.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { Autocomplete, InputChip } from '@skeletonlabs/skeleton';
+	import type { AutocompleteOption } from '@skeletonlabs/skeleton';
+	let inputChip = '';
+	export let inputChipList: string[] = [];
+	export let options: AutocompleteOption[] =[];
+    function onInputChipSelect(event: any): void {
+		console.log('onInputChipSelect', event.detail);
+		if (inputChipList.includes(event.detail.value) === false) {
+			inputChipList = [...inputChipList, event.detail.value];
+			inputChip = '';
+		}
+	}
+</script>
+
+<InputChip bind:input={inputChip} bind:value={inputChipList} name="chips" />
+
+<div class="card w-full max-h-48 p-4 overflow-y-auto">
+	<Autocomplete
+		bind:input={inputChip}
+		{options}
+		denylist={inputChipList}
+		on:selection={onInputChipSelect}
+	/>
+</div>
