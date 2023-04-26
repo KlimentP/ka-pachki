dbeb2059b141d0301b9240a7334d43bb51045669
Kliment Preshlenov <klimen.preshlenov@gmail>
2023-04-26 11:56:18 +0300

    add database types

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js'
 import { env } from '$env/dynamic/public';
+import type { Database } from '../supabase';
 
-export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
+export const supabaseClient = createClient<Database>(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
---
+++ src/supabase.ts
@@ -0,0 +1,140 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      designs: {
+        Row: {
+          color_scheme: string[]
+          id: number
+          material: string
+          name: string
+          preferred_emplopyee: number | null
+        }
+        Insert: {
+          color_scheme: string[]
+          id?: number
+          material: string
+          name: string
+          preferred_emplopyee?: number | null
+        }
+        Update: {
+          color_scheme?: string[]
+          id?: number
+          material?: string
+          name?: string
+          preferred_emplopyee?: number | null
+        }
+      }
+      employees: {
+        Row: {
+          id: number
+          name: string
+        }
+        Insert: {
+          id?: number
+          name: string
+        }
+        Update: {
+          id?: number
+          name?: string
+        }
+      }
+      orders: {
+        Row: {
+          closed_by: string | null
+          created_by: string | null
+          customer_id: string | null
+          date_closed: string | null
+          date_created: string | null
+          date_updated: string | null
+          deadline: string | null
+          design_id: number | null
+          duration_order: number | null
+          notes: string | null
+          order_id: string
+          quantity: number | null
+          status: string | null
+          units_already_produced: number
+          urgent: boolean
+        }
+        Insert: {
+          closed_by?: string | null
+          created_by?: string | null
+          customer_id?: string | null
+          date_closed?: string | null
+          date_created?: string | null
+          date_updated?: string | null
+          deadline?: string | null
+          design_id?: number | null
+          duration_order?: number | null
+          notes?: string | null
+          order_id?: string
+          quantity?: number | null
+          status?: string | null
+          units_already_produced?: number
+          urgent?: boolean
+        }
+        Update: {
+          closed_by?: string | null
+          created_by?: string | null
+          customer_id?: string | null
+          date_closed?: string | null
+          date_created?: string | null
+          date_updated?: string | null
+          deadline?: string | null
+          design_id?: number | null
+          duration_order?: number | null
+          notes?: string | null
+          order_id?: string
+          quantity?: number | null
+          status?: string | null
+          units_already_produced?: number
+          urgent?: boolean
+        }
+      }
+    }
+    Views: {
+      orders_full: {
+        Row: {
+          closed_by: string | null
+          color_scheme: string[] | null
+          created_by: string | null
+          created_by_email: string | null
+          customer_id: string | null
+          date_closed: string | null
+          date_created: string | null
+          date_updated: string | null
+          deadline: string | null
+          design_id: number | null
+          duration_order: number | null
+          id: number | null
+          material: string | null
+          name: string | null
+          notes: string | null
+          order_id: string | null
+          preferred_emplopyee: number | null
+          quantity: number | null
+          status: string | null
+          units_already_produced: number | null
+          urgent: boolean | null
+        }
+      }
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
