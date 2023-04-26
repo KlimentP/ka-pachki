9e63171331986a895cb133fb2e78d3d691e66fc7
Kliment Preshlenov <klimen.preshlenov@gmail>
2023-04-26 19:10:13 +0300

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div
+	class="container h-full mx-auto flex flex-col gap-4 justify-center items-center max-sm:p-4"
+>       <h1 class="text-slate-800">UH OH</h1>
+		<h2 class="text-slate-800 italic">{$page.status} {$page?.error?.message}</h2>
+		<span class="text-xl">
+			<img class="h-96 rounded-lg" src="/error.png" alt={$page.status.toString()} />
+		</span>
+</div>
