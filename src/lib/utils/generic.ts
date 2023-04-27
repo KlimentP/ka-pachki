567332403bd379c44783d473e515794323c2bf13
Kliment Preshlenov <klimen.preshlenov@gmail>
2023-04-27 15:35:58 +0300

    add dbtoautocomplete func in utils/generics

--- src/lib/utils/generic.ts
+++ src/lib/utils/generic.ts
@@ -1,3 +1,9 @@
 export const capitalizeString = (s: string) => {
     return s.charAt(0).toUpperCase() + s.slice(1);
 }
+
+export const dbToAutocomplete = (items: {id: string | number, name:string}[])=>{
+    return items.map((item) => {
+		return { value: item.id, label: capitalizeString(item.name) };}
+	)
+}
