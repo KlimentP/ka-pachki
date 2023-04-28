b1da1f98f9a97a021105aacd5bed6e26b5f93f8e
Kliment Preshlenov <klimen.preshlenov@gmail>
2023-04-28 10:25:10 +0300

    fix single autocomplete

--- src/lib/components/AutoCompleteSingle.svelte
+++ src/lib/components/AutoCompleteSingle.svelte
@@ -3,16 +3,21 @@
 	import type { AutocompleteOption } from '@skeletonlabs/skeleton';
 
 let input = ''
+let actualValue = '';
 export let options: AutocompleteOption[] = [];
+export let formName: string;
 function onSelection(event: any): void {
 	input = event.detail.label;
+	actualValue = event.detail.value;
 }
 
 
 </script>
 
 
-<input class="input" type="search" name="demo" bind:value={input} placeholder="Search..." />
+<input class="input" type="search" name={formName} bind:value={input} placeholder="Search..." />
+<input class="input"  name={formName} bind:value={actualValue}  hidden/>
+
 <div class="card w-full  max-h-48 p-4 overflow-y-auto">
-	<Autocomplete bind:input={input} options={options} on:selection={onSelection} />
+	<Autocomplete bind:input={input} options={options} on:selection={onSelection}  />
 </div>
