dd26b59d3e28bc0930f35445c26961a24e9caa70
Kliment Preshlenov <klimen.preshlenov@gmail>
2023-04-29 23:54:40 +0300

    add tooltip component

---
+++ src/lib/components/TooltipButton.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { PopupSettings } from '@skeletonlabs/skeleton';
+	import { popup } from '@skeletonlabs/skeleton';
+
+	export let popupText = '';
+    export let target: string;
+	let popupSettings: PopupSettings = {
+		// Set the event as: click | hover | hover-click | focus | focus-click
+		event: 'hover' ,
+		placement: 'top',
+		// Provide a matching 'data-popup' value.
+		target,
+		closeQuery: '',
+	};
+</script>
+
+<div>
+	<div use:popup={popupSettings}>
+		<slot name="button"></slot>
+	</div>
+	<div data-popup={target}>
+		<slot name="tooltip">{popupText}</slot>
+	</div>
+</div>
