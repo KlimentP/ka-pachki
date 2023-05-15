43b877ac536fc21984a2115001b3f0449acfd552
Kliment Preshlenov <klimen.preshlenov@gmail>
2023-05-15 10:56:51 +0300

    custom ColorScheme component

---
+++ src/lib/components/ColorScheme.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { colorMap } from '$lib/utils/colorMap';
+	export let colors: string[];
+</script>
+
+<div class="flex flex-wrap gap-2 py-4">
+	{#each colors as color}
+		<div
+			style="border-color: {colorMap[color]}"
+			class="chip chip-hover rounded-xl text-slate-800 border-2 shadow-lg"
+		>
+			{color}
+		</div>
+	{/each}
+</div>
