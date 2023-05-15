2e83fae3415473493ff574bde8220630c23963e2
Kliment Preshlenov <klimen.preshlenov@gmail>
2023-05-15 13:44:35 +0300

    use selectOrders util in planner page.server

--- src/routes/planner/+page.server.ts
+++ src/routes/planner/+page.server.ts
@@ -1,6 +1,8 @@
 
+import  {selectOrders} from '$lib/utils/supabase/selectOrders';
+
 export async function load({ locals }) {
-	const { data: orders } = await locals.supabase.from('orders_full').select('*');
+	const orders = await selectOrders();
 	const { data: employees } = await locals.supabase.from('employees').select('name,id');
 	return { orders: orders ?? [], employees: employees ?? [] };
 }
