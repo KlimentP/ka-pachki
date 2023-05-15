df4d6a357096b28817b37137308c417b691e0758
Kliment Preshlenov <klimen.preshlenov@gmail>
2023-05-15 23:10:56 +0300

    separate component to visualize material

---
+++ src/lib/components/Material.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { Material } from '$lib/types';
+	import TooltipButton from '$lib/components/TooltipButton.svelte';
+	import Icon from '@iconify/svelte';
+	export let material: Material;
+    export let iconHeight = "24";
+
+	const materialDict = {
+		embossed_lid: 'tabler:texture',
+		label: 'quill:label',
+		butter: 'fluent-emoji-high-contrast:butter',
+		lid: 'openmoji:jar',
+		uv_butter: 'tabler:uv-index'
+	};
+</script>
+<div class="w-fit">
+<TooltipButton popupText={material}>
+	<svelte:fragment slot="button">
+		<Icon height={iconHeight} icon={materialDict[material]} />
+	</svelte:fragment>
+</TooltipButton>
+</div>
