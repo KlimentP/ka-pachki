3526fe5b7fa59859f1f890cdb24db81eb1270c11
Kliment Preshlenov <klimen.preshlenov@gmail>
2023-05-15 23:11:21 +0300

    fix tooltip bug, format code

--- src/lib/components/TooltipButton.svelte
+++ src/lib/components/TooltipButton.svelte
@@ -3,22 +3,33 @@
 	import { popup } from '@skeletonlabs/skeleton';
 
 	export let popupText = '';
-    export let target: string;
+	export let target = '';
+	if (!target) {
+		target =
+			Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+	}
 	let popupSettings: PopupSettings = {
 		// Set the event as: click | hover | hover-click | focus | focus-click
-		event: 'hover' ,
+		event: 'hover',
 		placement: 'top',
 		// Provide a matching 'data-popup' value.
 		target,
-		closeQuery: '',
+		closeQuery: ''
 	};
 </script>
 
 <div>
 	<div use:popup={popupSettings}>
-		<slot name="button"></slot>
+		<slot name="button" />
 	</div>
 	<div data-popup={target}>
-		<slot name="tooltip">{popupText}</slot>
+		<slot name="tooltip">
+			<div
+				class="bg-slate-200 p-4 rounded-lg border-2
+			align-middle mx-auto justify-center items-center shadow-lg"
+			>
+				{popupText}
+			</div>
+		</slot>
 	</div>
 </div>
