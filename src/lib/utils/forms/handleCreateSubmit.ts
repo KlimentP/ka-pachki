7cb95d01bb858da28d2c14585a22ff7211f3ea6a
Kliment Preshlenov <klimen.preshlenov@gmail>
2023-05-23 16:41:25 +0300

    handleCreateSubmit supports creator field and also protects action from unauthed users

--- src/lib/utils/forms/handleCreateSubmit.ts
+++ src/lib/utils/forms/handleCreateSubmit.ts
@@ -1,28 +1,38 @@
-
 import { message, setError, superValidate } from 'sveltekit-superforms/server';
 import { removeNullValues, capitalizeString } from '$lib/utils/generic';
-import { fail } from '@sveltejs/kit';
+import { error, fail } from '@sveltejs/kit';
 
-export const handleCreateSubmit = (schema: any, resource: string) => {
-
-    return async ({ request, locals }) => {
-    // const formData = await request.formData();
-    const form = await superValidate(request, schema);
-    if (!form.valid) return fail(400, { form });
-    const submitData = removeNullValues(form.data)
-    if (!form.data.id) {
-        // CREATE
-        const { error } = await locals.supabase.from(resource).insert([submitData]);
-        if (error) {
-            return setError(form, null, error.message);
-        }
-        return message(form, `${capitalizeString(resource)} created!`);
-    } else {
-        // UPDATE
-        const { error } = await locals.supabase.from(resource).update(submitData).eq("id", form.data.id );
-        if (error) {
-            return setError(form, null, error.message);
-        }
-        return message(form, `${capitalizeString(resource)} updated!`);
-    }
-}}
+export const handleCreateSubmit = (schema: any, resource: string, withCreator = false) => {
+	return async ({ request, locals: { supabase, getSession } }) => {
+		const session = await getSession();
+		if (!session) {
+			// the user is not signed in
+			throw error(401, { message: 'Unauthorized' });
+		}
+		const form = await superValidate(request, schema);
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+		let submitData = removeNullValues(form.data);
+		if (withCreator) {
+			const session = await getSession();
+			const creator = { created_by: session?.user.id };
+			submitData = { ...submitData, ...creator };
+		}
+		if (!form.data.id) {
+			// CREATE
+			const { error } = await supabase.from(resource).insert([submitData]);
+			if (error) {
+				return setError(form, null, error.message);
+			}
+			return message(form, `${capitalizeString(resource)} created!`);
+		} else {
+			// UPDATE
+			const { error } = await supabase.from(resource).update(submitData).eq('id', form.data.id);
+			if (error) {
+				return setError(form, null, error.message);
+			}
+			return message(form, `${capitalizeString(resource)} updated!`);
+		}
+	};
+};
