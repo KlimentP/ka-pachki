8ab82ad15be96d5572a117a67cff3ce1241371e1
Kliment Preshlenov <klimen.preshlenov@gmail>
2023-05-23 22:58:56 +0300

    sort orders by date_created in selectOrders

--- src/lib/utils/supabase/selectOrders.ts
+++ src/lib/utils/supabase/selectOrders.ts
@@ -1,22 +1,30 @@
 import { supabaseClient } from '$lib/supabaseClient';
 import { calculateMinutesLength } from '../materialCalcs';
-import type { Material } from "$lib/types";
+import type { Material } from '$lib/types';
 
 import type { Database } from '../../../supabase';
 
 type CalcedOrder = Database['public']['Views']['orders_full']['Row'] & {
-    minutes_length: number;
-    material: Material;
-  }
+	minutes_length: number;
+	material: Material;
+};
 
 export async function selectOrders() {
-	const { data: orders }  = await supabaseClient.from('orders_full').select('*') as {data: CalcedOrder[]};
-    // substract units_already_produced from quantity to get the remaining units to produce and then convert to minutes
+	const { data: orders } = (await supabaseClient
+		.from('orders_full')
+		.select('*')
+		.order('quantity', { ascending: false })) as { data: CalcedOrder[] };
+
+	// substract units_already_produced from quantity to get the remaining units to produce and then convert to minutes
 	orders?.map((order) => {
 		const unitsRemaining = (order.quantity ?? 0) - (order.units_already_produced ?? 0);
-		order.minutes_length = order.quantity && order.material && unitsRemaining > 0
-			? calculateMinutesLength(order.quantity - (order.units_already_produced ?? 0), order.material)
-			: 0;
+		order.minutes_length =
+			order.quantity && order.material && unitsRemaining > 0
+				? calculateMinutesLength(
+						order.quantity - (order.units_already_produced ?? 0),
+						order.material
+				  )
+				: 0;
 		order.name = order.design_name;
 		return order;
 	});
