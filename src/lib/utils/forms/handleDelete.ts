8dd0f6fd995e0ec7946205cc82dfa91925d3a3c2
Kliment Preshlenov <klimen.preshlenov@gmail>
2023-05-23 16:41:41 +0300

    protect handleDelete from unauth

--- src/lib/utils/forms/handleDelete.ts
+++ src/lib/utils/forms/handleDelete.ts
@@ -1,9 +1,15 @@
 import { handleModalFormErrors } from "./generic";
+import { error } from '@sveltejs/kit';
 
 export const handleDelete = (resource: string) => {
-    return async ({ request, locals }) => {
+    return async ({ request, locals: { supabase, getSession } }) => {
+        const session = await getSession()
+        if (!session) {
+            // the user is not signed in
+            throw error(401, { message: 'Unauthorized' })
+          }
         const data = await request.formData();
-        const { error } = await locals.supabase.from(resource).delete().eq('id', data.get('id'));
-        handleModalFormErrors(error);
+        const { err } = await supabase.from(resource).delete().eq('id', data.get('id'));
+        handleModalFormErrors(err);
     }
 }
