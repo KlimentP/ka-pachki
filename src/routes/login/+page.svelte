2e0c3504d44dbd9b3f938e45e71d50cb85cfa64c
Kliment Preshlenov <klimen.preshlenov@gmail>
2023-06-28 21:38:20 +0300

    custom login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,21 +1,37 @@
 <script lang="ts">
-	import { Auth } from '@supabase/auth-ui-svelte';
-	import { page } from '$app/stores';
-	import { ThemeSupa } from '@supabase/auth-ui-shared';
+	import { enhance } from '$app/forms';
+	export let form;
 </script>
 
 <div class="container h-full mx-auto flex flex-col justify-center items-center gap-2">
-	<div class="row flex-center flex">
-		<div class="col-6 text-slate-600 rounded-lg">
-			
-			<Auth
-				supabaseClient={$page.data.supabase}
-				view="sign_in"
-				redirectTo={`/logging-in?redirect=/`}
-				showLinks={false}
-				appearance={{theme: ThemeSupa}}
-				/>
+	<form class="form flex flex-col gap-4" method="post" use:enhance>
+		<div>
+			<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="email">
+				Email
+			</label>
+
+			<input class="input" type="text" name="email" value={form?.email ?? ''} />
 		</div>
-	</div>
-    <a href="/signup">Not registered? Sign up here</a>
+		<div>
+			<label
+				class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+				for="password"
+			>
+				Password
+			</label>
+			<input class="input" type="password" name="password" />
+		</div>
+		<button
+			class="bg-primary-500 hover:bg-primary-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+			type="submit"
+			>Log in
+		</button>
+		{#if form?.error}
+			<div class="alert variant-filled-error">
+				{form?.error}
+			</div>
+		{/if}
+	</form>
+
+	<a href="/signup">Not registered? Sign up here</a>
 </div>
